b039da32056777ff813e549d8ec92f6615993be7
unknown <formylucklife@keemail.me>
2025-07-02 19:17:36 +0800

    window

--- 进程枚举/4.枚举带窗口进程/4.枚举带窗口进程.cpp
+++ 进程枚举/4.枚举带窗口进程/4.枚举带窗口进程.cpp
@@ -6,11 +6,12 @@ BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam) {
     SetConsoleOutputCP(CP_UTF8);
     std::locale::global(std::locale(""));
     DWORD pid;
-    GetWindowThreadProcessId(hwnd, &pid);
+    if (GetWindowThreadProcessId(hwnd, &pid)) {//如果出错， 返回0,获取窗口进程ID
 
-    TCHAR title[256];
-    if (GetWindowText(hwnd, title, 256)) {
-        _tprintf(TEXT("窗口句柄: %p\t进程ID: %u\t标题: %s\n"), hwnd, pid, title);
+        TCHAR title[256];
+        if (GetWindowText(hwnd, title, 256)) {
+            _tprintf(TEXT("窗口句柄: %p\t进程ID: %u\t标题: %s\n"), hwnd, pid, title);
+        }
     }
     return TRUE;
 }
